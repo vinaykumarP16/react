3d46e866e37bd4dd6a842b86e2fe1556ec92a8dc
vinaykumarP16 <vinaykumar.p1995v@gmail.com>
2021-01-17 16:48:55 +0530

    inital commit

--- src/App.js
+++ src/App.js
@@ -7,16 +7,10 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          my react app
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+     <h1> 9591565437 </h1>  
+     <h1> vinaykumar.p1995v@gmail.com </h1>
       </header>
     </div>
   );
